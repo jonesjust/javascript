c31ec4f823a90782cf5153334609ff63f1aeea16
jjones254 <86092532+jjones254@users.noreply.github.com>
2023-01-15 08:59:01 -0600

    update times in estimateStudyTime.ts

--- estimateStudyTime.ts
+++ estimateStudyTime.ts
@@ -1,6 +1,6 @@
 let hours: number = 0;
 let minutes: number = 0;
-const arrayOfTimes: Array<number[]> = [[1, 0], [2, 40], [3, 20], [0, 60], [0, 120], [0, 120], [1, 726]];
+const arrayOfTimes: Array<number[]> = [[0, 33], [1, 34], [2, 1], [1, 25], [1, 3], [0, 37], [0, 54], [0, 57], [0, 27], [0, 39]];
 
 function addTime(addHours: number, addMinutes: number): void {
     minutes += addMinutes;
